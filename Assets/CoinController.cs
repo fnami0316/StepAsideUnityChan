b67714ccfa1c0529e9b9a69983a58bb1cd0b51e6
藤浪 裕貴 <fnami0316@gmail.com>
2019-12-09 23:35:59 +0900

    [fix]シーンビューに映っていてもオブジェクトが削除されるように変更

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class CoinController : MonoBehaviour {
 
+    // メインカメラのオブジェクト
+    private GameObject mainCamera;
+
     // Use this for initialization
     void Start() {
+        // メインカメラのオブジェクトを取得
+        this.mainCamera = GameObject.Find("Main Camera");
+
         //回転を開始する角度を設定
         this.transform.Rotate(0, Random.Range(0, 360), 0);
     }
 
     // Update is called once per frame
     void Update() {
+        // 本オブジェクトのz座標がメインカメラのz座標より小さい場合オブジェクトを削除
+        if(this.gameObject.transform.position.z < this.mainCamera.transform.position.z) Destroy(this.gameObject);
+
         //回転
         this.transform.Rotate(0, 3, 0);
     }
 
-    // カメラに表示されなくなったことをトリガーとして実行される
-    private void OnBecameInvisible() {
-        Destroy(this.gameObject);
-    }
 }
--- Assets/TrafficConeController.cs
+++ Assets/TrafficConeController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class TrafficConeController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // メインカメラのオブジェクト
+    private GameObject mainCamera;
+
+    // Use this for initialization
+    void Start () {
+        // メインカメラのオブジェクトを取得
+        this.mainCamera = GameObject.Find("Main Camera");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-    private void OnBecameInvisible() {
-        Destroy(this.gameObject);
+        // 本オブジェクトのz座標がメインカメラのz座標より小さい場合オブジェクトを削除
+        if(this.gameObject.transform.position.z < this.mainCamera.transform.position.z) Destroy(this.gameObject);
     }
+
 }
