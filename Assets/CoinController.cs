fca8fd5230ff7d47df6aa8d171106bc51c5023eb
藤浪 裕貴 <fnami0316@gmail.com>
2019-12-11 01:23:45 +0900

    [fix]CarController, ConeControllerを削除してItemDeleterを新規作成して、オブジェクト削除処理を移動させた。メインカメラのオブジェクト取得方法を変更

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class CoinController : MonoBehaviour {
 
-    // メインカメラのオブジェクト
-    private GameObject mainCamera;
-
     // Use this for initialization
     void Start() {
-        // メインカメラのオブジェクトを取得
-        this.mainCamera = GameObject.Find("Main Camera");
 
         //回転を開始する角度を設定
         this.transform.Rotate(0, Random.Range(0, 360), 0);
@@ -17,8 +12,6 @@ public class CoinController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // 本オブジェクトのz座標がメインカメラのz座標より小さい場合オブジェクトを削除
-        if(this.gameObject.transform.position.z < this.mainCamera.transform.position.z) Destroy(this.gameObject);
 
         //回転
         this.transform.Rotate(0, 3, 0);
---
+++ Assets/ItemDeleter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDeleter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // 本オブジェクトのz座標がメインカメラのz座標より小さい場合オブジェクトを削除
+        if(this.gameObject.transform.position.z < Camera.main.transform.position.z) Destroy(this.gameObject);
+    }
+}
